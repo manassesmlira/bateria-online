c0479676d4bf44417773d9ebe5e848ea396083b2
Manasses_M_Lira <manassesmlira@gmail.com>
2023-05-01 13:57:14 -0300

    bateria funcionando

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ document.body.addEventListener('keyup', (event)=>{
 playSound(event.code.toLowerCase())
 })
 
+document.querySelector('.composer button').addEventListener('click', ()=>{
+let song = document.querySelector('#input').value
+
+    if(song !== "") {
+        let songArray = song.split('')
+        playComposition(songArray)
+    }
+})
+
+
+
 
 
 function playSound(sound) {
@@ -19,5 +30,20 @@ function playSound(sound) {
         setTimeout( ()=>{
             keyElement.classList.remove('active')
         }, 300 )
+    }
+}
+
+
+function playComposition(songArray) {
+    let wait = 0
+    
+    for(let songItem of songArray) {
+        setTimeout(()=>{
+            playSound(`key${songItem}`)
+        }, wait)
+        
+        wait += 250
+
+        
     }
 }
